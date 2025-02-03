cb86c0a50ab9bb781e04c54f8898a715a4fc5a50
Robert Stypa <robert.stypa@nordicsemi.no>
2025-02-03 15:26:21 +0100

    feat: add support for generating secondary B image in case of cpunet refactor: code cleanup
    Signed-off-by: Robert Stypa <robert.stypa@nordicsemi.no>

--- sysbuild/suit.cmake
+++ sysbuild/suit.cmake
@@ -41,43 +41,48 @@ if(SB_CONFIG_SUIT_BUILD_RECOVERY)
 endif()
 
 if(SB_CONFIG_SUIT_BUILD_AB_UPDATE)
-  set(image ${DEFAULT_IMAGE}_b)
-#  ExternalNcsVariantProject_Add(APPLICATION ${DEFAULT_IMAGE} VARIANT ${image})
+  
+  set(SAMPLE_SOURCE_DIR ${APP_DIR})
+
+  set(APP_IMAGE ${DEFAULT_IMAGE}_slot_b)
 
   string(REPLACE "/" ";" split_board_qualifiers "${BOARD_QUALIFIERS}")
   list(GET split_board_qualifiers 1 target_soc)
   list(GET split_board_qualifiers 2 target_cpucluster)
 
-  set(board_target "${BOARD}/${target_soc}/${target_cpucluster}")
+  set(BOARD_TARGET "${BOARD}/${target_soc}/${target_cpucluster}")
   
-  message(WARNING "APPLICATION_SOURCE_DIR: ${APPLICATION_SOURCE_DIR}")
-  message(WARNING "APPLICATION_DIR: ${APPLICATION_DIR}")
-  message(WARNING "ZEPHYR_BASE: ${ZEPHYR_BASE}")
-  message(WARNING "CMAKE_CURRENT_SOURCE_DIR: ${CMAKE_CURRENT_SOURCE_DIR}")
-  message(WARNING "CMAKE_SOURCE_DIR: ${CMAKE_SOURCE_DIR}")
 
   ExternalZephyrProject_Add(
-    APPLICATION "${image}"
-    SOURCE_DIR "${ZEPHYR_NRF_MODULE_DIR}/samples/suit/suit_ab_images"
-    BOARD ${board_target}
+    APPLICATION ${APP_IMAGE}
+    SOURCE_DIR ${SAMPLE_SOURCE_DIR}
+    BOARD ${BOARD_TARGET}
     BOARD_REVISION ${BOARD_REVISION}
   )
 
-  set_config_bool("${image}" CONFIG_SUIT_LOCAL_ENVELOPE_GENERATE y)
-  # TODO: filename and target names should be handled by sysbuild/suit.cmake during the generation of suit envelopes
-  # TODO2: the goal is to have only one envelope for both images!
-  # Template rendered does not know about current image,
-  #   so from template renderer all variables are equal and is not possible to detect 'image_b' case
-  # set_config_string("${image}" CONFIG_SUIT_ENVELOPE_TEMPLATE_FILENAME "app_envelope.yaml.jinja2")
-  sysbuild_get(target IMAGE ${DEFAULT_IMAGE} VAR CONFIG_SUIT_ENVELOPE_TARGET KCONFIG)
-  # TODO: target name should be handled by sysbuild/suit.cmake during the generation of suit envelopes
-  set_config_string("${image}" CONFIG_SUIT_ENVELOPE_TARGET "application_b")
-  set_config_bool("${image}" CONFIG_NRF_REGTOOL_GENERATE_UICR n)
-  # Force application to do not use DT code partition
-  # set_config_bool("${image}" CONFIG_USE_DT_CODE_PARTITION n)
-  # set_property(TARGET ${image} APPEND_STRING PROPERTY CONFIG "CONFIG_FLASH_LOAD_OFFSET=0xDE800\n")
+  set_config_string(${APP_IMAGE} CONFIG_SUIT_ENVELOPE_TARGET "application_b")
+  set_config_bool(${APP_IMAGE} CONFIG_NRF_REGTOOL_GENERATE_UICR n)
   
-  # TODO: remove, added only for testing
-  set_property(TARGET ${image} APPEND_STRING PROPERTY CONFIG "CONFIG_N_BLINKS=10\n")
-  add_overlay_dts(${image} "${ZEPHYR_NRF_MODULE_DIR}/samples/suit/suit_ab_images/sysbuild/ab_images_b.overlay")
+  set_property(TARGET ${APP_IMAGE} APPEND_STRING PROPERTY CONFIG "CONFIG_N_BLINKS=10\n")
+  add_overlay_dts(${APP_IMAGE} "${SAMPLE_SOURCE_DIR}/sysbuild/ab_images_b.overlay")
+
+  if(NOT (SB_CONFIG_NETCORE_NONE OR SB_CONFIG_NETCORE_EMPTY) )
+    set(NET_IMAGE ${SB_CONFIG_NETCORE_IMAGE_NAME}_slot_b)
+    set(NET_BOARD_TARGET "${BOARD}/${target_soc}/${SB_CONFIG_NETCORE_REMOTE_BOARD_TARGET_CPUCLUSTER}")
+    set(NET_SAMPLE_SOURCE_DIR ${SB_CONFIG_NETCORE_IMAGE_PATH})
+
+    ExternalZephyrProject_Add(
+      APPLICATION ${NET_IMAGE}
+      SOURCE_DIR ${NET_SAMPLE_SOURCE_DIR}
+      BOARD ${NET_BOARD_TARGET}
+      BOARD_REVISION ${BOARD_REVISION}
+    )    
+
+    set_config_string(${NET_IMAGE} CONFIG_SUIT_ENVELOPE_TARGET "radio_b")
+    set_config_bool(${NET_IMAGE} CONFIG_NRF_REGTOOL_GENERATE_UICR n)
+    
+    add_overlay_dts(${NET_IMAGE} "${SAMPLE_SOURCE_DIR}/sysbuild/hci_ipc_b.overlay")   
+    add_overlay_dts(${NET_IMAGE} "${SAMPLE_SOURCE_DIR}/sysbuild/ab_images_b_net.overlay")     
+  endif()
+  
 endif()
