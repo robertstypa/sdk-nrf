1b03c0567ecb38b03b518f317c9a51fb97c63efe
Robert Stypa <robert.stypa@nordicsemi.no>
2025-02-03 08:40:19 +0100

    feat: add support for generating secondary B image
    Signed-off-by: Robert Stypa <robert.stypa@nordicsemi.no>

--- sysbuild/suit.cmake
+++ sysbuild/suit.cmake
@@ -49,24 +49,35 @@ if(SB_CONFIG_SUIT_BUILD_AB_UPDATE)
   list(GET split_board_qualifiers 2 target_cpucluster)
 
   set(board_target "${BOARD}/${target_soc}/${target_cpucluster}")
+  
+  message(WARNING "APPLICATION_SOURCE_DIR: ${APPLICATION_SOURCE_DIR}")
+  message(WARNING "APPLICATION_DIR: ${APPLICATION_DIR}")
+  message(WARNING "ZEPHYR_BASE: ${ZEPHYR_BASE}")
+  message(WARNING "CMAKE_CURRENT_SOURCE_DIR: ${CMAKE_CURRENT_SOURCE_DIR}")
+  message(WARNING "CMAKE_SOURCE_DIR: ${CMAKE_SOURCE_DIR}")
 
   ExternalZephyrProject_Add(
     APPLICATION "${image}"
-    SOURCE_DIR "${ZEPHYR_NRF_MODULE_DIR}/samples/suit/smp_transfer"
+    SOURCE_DIR "${ZEPHYR_NRF_MODULE_DIR}/samples/suit/suit_ab_images"
     BOARD ${board_target}
     BOARD_REVISION ${BOARD_REVISION}
   )
 
-  # Dodaj overlay po utworzeniu projektu
-  # add_overlay_dts(${image} "${CMAKE_CURRENT_LIST_DIR}/ab_update.overlay")
-
   set_config_bool("${image}" CONFIG_SUIT_LOCAL_ENVELOPE_GENERATE y)
-  set_config_string("${image}" CONFIG_SUIT_ENVELOPE_TEMPLATE_FILENAME "app_b_envelope.yaml.jinja2")
+  # TODO: filename and target names should be handled by sysbuild/suit.cmake during the generation of suit envelopes
+  # TODO2: the goal is to have only one envelope for both images!
+  # Template rendered does not know about current image,
+  #   so from template renderer all variables are equal and is not possible to detect 'image_b' case
+  # set_config_string("${image}" CONFIG_SUIT_ENVELOPE_TEMPLATE_FILENAME "app_envelope.yaml.jinja2")
   sysbuild_get(target IMAGE ${DEFAULT_IMAGE} VAR CONFIG_SUIT_ENVELOPE_TARGET KCONFIG)
+  # TODO: target name should be handled by sysbuild/suit.cmake during the generation of suit envelopes
   set_config_string("${image}" CONFIG_SUIT_ENVELOPE_TARGET "application_b")
   set_config_bool("${image}" CONFIG_NRF_REGTOOL_GENERATE_UICR n)
-  set_config_bool("${image}" CONFIG_USE_DT_CODE_PARTITION n)
-  #set_config_string("${image}" CONFIG_FLASH_LOAD_OFFSET "0xc1000")
-  set_property(TARGET ${image} APPEND_STRING PROPERTY CONFIG "CONFIG_FLASH_LOAD_OFFSET=0xD7000\n")
+  # Force application to do not use DT code partition
+  # set_config_bool("${image}" CONFIG_USE_DT_CODE_PARTITION n)
+  # set_property(TARGET ${image} APPEND_STRING PROPERTY CONFIG "CONFIG_FLASH_LOAD_OFFSET=0xDE800\n")
+  
+  # TODO: remove, added only for testing
   set_property(TARGET ${image} APPEND_STRING PROPERTY CONFIG "CONFIG_N_BLINKS=10\n")
+  add_overlay_dts(${image} "${ZEPHYR_NRF_MODULE_DIR}/samples/suit/suit_ab_images/sysbuild/ab_images_b.overlay")
 endif()
