5f1b523f572a1f0e31b6b9dec2887992ec1b5f3a
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2025-02-12 16:06:17 +0100

    suit: Autogenerate B slots
    Based on the <image>_slot_b.overlay, invoke the sysbuild logic for
    automatic B slot generation.
    
    Ref: NCSDK-30812
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- sysbuild/suit.cmake
+++ sysbuild/suit.cmake
@@ -41,48 +41,22 @@ if(SB_CONFIG_SUIT_BUILD_RECOVERY)
 endif()
 
 if(SB_CONFIG_SUIT_BUILD_AB_UPDATE)
+  cmake_path(APPEND APP_DIR "sysbuild" OUTPUT_VARIABLE slot_overlay_dir)
 
-  set(SAMPLE_SOURCE_DIR ${APP_DIR})
+  get_property(images GLOBAL PROPERTY sysbuild_images)
+  foreach(image ${images})
+    cmake_path(APPEND slot_overlay_dir "${image}_slot_b.overlay" OUTPUT_VARIABLE overlay)
+    if(NOT EXISTS ${overlay})
+      continue()
+    else()
+      message(STATUS "Generate slot B for image: ${image}")
+    endif()
 
-  set(APP_IMAGE ${DEFAULT_IMAGE}_slot_b)
-
-  string(REPLACE "/" ";" split_board_qualifiers "${BOARD_QUALIFIERS}")
-  list(GET split_board_qualifiers 1 target_soc)
-  list(GET split_board_qualifiers 2 target_cpucluster)
-
-  set(BOARD_TARGET "${BOARD}/${target_soc}/${target_cpucluster}")
-
-  ExternalZephyrProject_Add(
-    APPLICATION ${APP_IMAGE}
-    SOURCE_DIR ${SAMPLE_SOURCE_DIR}
-    BOARD ${BOARD_TARGET}
-    BOARD_REVISION ${BOARD_REVISION}
-  )
-
-  set_config_string(${APP_IMAGE} CONFIG_SUIT_ENVELOPE_TARGET "application_b")
-  set_config_bool(${APP_IMAGE} CONFIG_NRF_REGTOOL_GENERATE_UICR n)
-
-  # Choose slot_b_partition as the active code partition
-  add_overlay_dts(${APP_IMAGE} "${SAMPLE_SOURCE_DIR}/sysbuild/ab_images_b.overlay")
-
-  if(NOT (SB_CONFIG_NETCORE_NONE OR SB_CONFIG_NETCORE_EMPTY) )
-    set(NET_IMAGE ${SB_CONFIG_NETCORE_IMAGE_NAME}_slot_b)
-    set(NET_BOARD_TARGET "${BOARD}/${target_soc}/${SB_CONFIG_NETCORE_REMOTE_BOARD_TARGET_CPUCLUSTER}")
-    set(NET_SAMPLE_SOURCE_DIR ${SB_CONFIG_NETCORE_IMAGE_PATH})
-
-    ExternalZephyrProject_Add(
-      APPLICATION ${NET_IMAGE}
-      SOURCE_DIR ${NET_SAMPLE_SOURCE_DIR}
-      BOARD ${NET_BOARD_TARGET}
-      BOARD_REVISION ${BOARD_REVISION}
-    )    
-
-    set_config_string(${NET_IMAGE} CONFIG_SUIT_ENVELOPE_TARGET "radio_b")
-    set_config_bool(${NET_IMAGE} CONFIG_NRF_REGTOOL_GENERATE_UICR n)
-
-    # Choose slot_b_partition as the active code partition and add required overlays
-    add_overlay_dts(${NET_IMAGE} "${SAMPLE_SOURCE_DIR}/sysbuild/hci_ipc.overlay")   
-    add_overlay_dts(${NET_IMAGE} "${SAMPLE_SOURCE_DIR}/sysbuild/ab_images_b_net.overlay")     
-  endif()
-
+    ExternalNcsVariantProject_Add(
+      APPLICATION ${image}
+      VARIANT "${image}_variant_b"
+    )
+    # Choose slot_b_partition as the active code partition
+    add_overlay_dts("${image}_variant_b" "${overlay}")
+  endforeach()
 endif()
