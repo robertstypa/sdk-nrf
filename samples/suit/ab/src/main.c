fbec6aab10bb00f2cfc066550539a18d944811c7
Sylwester Konczyk <sylwester.konczyk@nordicsemi.no>
2025-02-21 08:58:18 +0100

    suit: AB updates
    Sample application code
    
    Signed-off-by: Sylwester Konczyk <sylwester.konczyk@nordicsemi.no>

--- samples/suit/ab/src/main.c
+++ samples/suit/ab/src/main.c
@@ -4,42 +4,79 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
+#include <suit_manifests_state.h>
+#include <suit_components_state.h>
+#include <device_management.h>
+
+#include "common.h"
 #include <zephyr/kernel.h>
 #include <dk_buttons_and_leds.h>
-#ifdef CONFIG_MGMT_SUITFU
-#include <suitfu_mgmt.h>
-#endif /* CONFIG_MGMT_SUITFU */
-#include <zephyr/storage/flash_map.h>
-#include "common.h"
+#include <zephyr/logging/log.h>
 
-#ifdef CONFIG_SSF_SUIT_SERVICE_ENABLED
-#include <sdfw/sdfw_services/suit_service.h>
-#endif /* CONFIG_SSF_SUIT_SERVICE_ENABLED */
+LOG_MODULE_REGISTER(AB, CONFIG_SUIT_LOG_LEVEL);
 
-#define DFU_PARTITION_OFFSET  FIXED_PARTITION_OFFSET(dfu_partition)
-#define DFU_PARTITION_SIZE    FIXED_PARTITION_SIZE(dfu_partition)
-#define DFU_PARTITION_ADDRESS suit_plat_mem_nvm_ptr_get(DFU_PARTITION_OFFSET)
+static void button_handler(uint32_t button_state, uint32_t has_changed)
+{
+	if ((has_changed & DK_BTN4_MSK) && (button_state & DK_BTN4_MSK)) {
+		toggle_preferred_boot_set();
+	}
+}
 
 int main(void)
 {
-	int ret = dk_leds_init();
+	char *app_img_name = NULL;
+	int ret = 0;
 
+	ret = dk_leds_init();
 	if (ret) {
-		printk("Cannot init LEDs (err: %d)\r\n", ret);
+		LOG_ERR("Cannot init LEDs (err: %d)\r\n", ret);
 	}
 
-	printk("Hello world from %s blinks: %d, BUILD: %s %s\r\n", CONFIG_BOARD, CONFIG_N_BLINKS,
-	       __DATE__, __TIME__);
+	ret = dk_buttons_init(button_handler);
+	if (ret) {
+		LOG_ERR("Cannot init buttons (err: %d)", ret);
+	}
+
+	dk_set_led_on(DK_LED1);
+	dk_set_led_on(DK_LED2);
 
 	if (IS_ENABLED(CONFIG_MCUMGR_TRANSPORT_BT)) {
 		start_smp_bluetooth_adverts();
+		k_msleep(250);
 	}
+	printk("\n");
+
+	suit_mainfests_state_report();
+	printk("\n");
+
+	int blinking_led = DK_LED1;
+	if (DT_SAME_NODE(DT_ALIAS(suit_active_code_partition),
+			 DT_NODELABEL(cpuapp_slot_a_partition))) {
+		app_img_name = "A";
+	} else {
+		app_img_name = "B";
+		blinking_led = DK_LED2;
+	}
+
+	printk("---------------------------------------------------\n");
+	printk("A/B Hello world from %s, blinks: %d, img: %s\n", CONFIG_BOARD, CONFIG_N_BLINKS,
+	       app_img_name);
+	printk("BUILD: %s %s\n", __DATE__, __TIME__);
+	printk("---------------------------------------------------\n\n");
+
+	suit_components_state_report();
+	printk("\n");
+
+	device_boot_state_report();
+	printk("\n");
+
+	device_healthcheck();
 
 	while (1) {
 		for (int i = 0; i < CONFIG_N_BLINKS; i++) {
-			dk_set_led_on(DK_LED1);
+			dk_set_led_off(blinking_led);
 			k_msleep(250);
-			dk_set_led_off(DK_LED1);
+			dk_set_led_on(blinking_led);
 			k_msleep(250);
 		}
 
