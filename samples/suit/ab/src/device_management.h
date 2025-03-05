0c233fbf15e5d845676e20c50d4aa307cd5c25ab
Artur Hadasz <artur.hadasz@nordicsemi.no>
2025-03-05 15:42:43 +0100

    samples: suit ab: status leds + report recovery
    This commit adds more advanced status LEDs behavior
    and log reporting being in recovery mode.
    
    Signed-off-by: Artur Hadasz <artur.hadasz@nordicsemi.no>

--- samples/suit/ab/src/device_management.h
+++ samples/suit/ab/src/device_management.h
@@ -11,6 +11,20 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+#include <stdbool.h>
+
+/* Definitions below reflect a way of usage of manifest-controlled variables
+ * in this specific application.
+ */
+#define BOOT_STATUS_A	       1
+#define BOOT_STATUS_B	       2
+#define BOOT_STATUS_A_DEGRADED 3
+#define BOOT_STATUS_B_DEGRADED 4
+#define BOOT_STATUS_A_NO_RADIO 5
+#define BOOT_STATUS_B_NO_RADIO 6
+#define BOOT_STATUS_CANT_BOOT  7
+
 /** @brief Performs a self-tests and confirms
  * successful firmware update, if needed
  */
@@ -24,5 +38,13 @@ void toggle_preferred_boot_set();
  * boot preference and a boot status
  */
 void device_boot_state_report(void);
+
+/** @brief Checks if the device is in recovery mode
+ */
+bool device_is_in_recovery_mode(void);
+
+/** @brief Gets the boot status of the device
+ */
+uint32_t device_boot_status_get(void);
 
 #endif /* DEVICE_MANAGEMANT_H__ */
